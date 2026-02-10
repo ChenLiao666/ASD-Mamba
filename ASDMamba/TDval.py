f4ac2f58fc95e8fd86c908f395d2c9707351a185
ChenLiao666 <LC547858170@outlook.com>
2026-02-10 16:03:04 +0800

    Update TDval.py

--- ASDMamba/TDval.py
+++ ASDMamba/TDval.py
@@ -93,7 +93,6 @@ if config.network == 'asd':
     model.cuda()
 
 total_params = sum(p.numel() for p in model.parameters())
-print(f"Total number of parameters in the model: {total_params}")
 
 # Load the pre-trained model weights
 model.load_state_dict(torch.load('TD.pth', map_location=device))
